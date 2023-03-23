c06fdd14228f3c44219c892070807dc54851f40d
mykyta <nikitastelmashenko@gmail.com>
2023-03-23 11:33:51 +0200

    Updates

---
+++ precache-manifest.c2fda5356f642299e38cbace22d227f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4154ed544ba4188c3054",
+    "url": "/calculator/static/css/main.2808f542.chunk.css"
+  },
+  {
+    "revision": "4154ed544ba4188c3054",
+    "url": "/calculator/static/js/main.6cc85219.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "c524eae18f11c3db2b60",
+    "url": "/calculator/static/js/2.b3d41ea4.chunk.js"
+  },
+  {
+    "revision": "2db8e759381ed4fb3650d5be53ba8135",
+    "url": "/calculator/static/media/BwModelica-Light.2db8e759.woff2"
+  },
+  {
+    "revision": "95ec361b9e97e483685154217c8fdda4",
+    "url": "/calculator/static/media/BwModelica-Bold.95ec361b.woff2"
+  },
+  {
+    "revision": "c7110776e4194724be80529214bfb2d2",
+    "url": "/calculator/static/media/BwModelica-Regular.c7110776.woff2"
+  },
+  {
+    "revision": "3f046f5722da68e19d747edda80842ce",
+    "url": "/calculator/static/media/BwModelica-Regular.3f046f57.woff"
+  },
+  {
+    "revision": "6aff4180a20995e0ea206ff7c93cf3a4",
+    "url": "/calculator/static/media/BwModelica-Medium.6aff4180.woff"
+  },
+  {
+    "revision": "1807bd651455cb157d030a70a28615b0",
+    "url": "/calculator/static/media/BwModelica-Bold.1807bd65.woff"
+  },
+  {
+    "revision": "7d6313d3ead8f28de1ee30d6f7ddb6aa",
+    "url": "/calculator/static/media/BwModelica-Light.7d6313d3.woff"
+  },
+  {
+    "revision": "54608e1eb860ba4972e010e5b5ccfbe3",
+    "url": "/calculator/static/media/BwModelica-Medium.54608e1e.woff2"
+  },
+  {
+    "revision": "37b804dcf34f6025f70a4d518c5fe683",
+    "url": "/calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/calculator/precache-manifest.d3271e7f8f2a2722f403fe92d5b7050d.js"
+  "/calculator/precache-manifest.c2fda5356f642299e38cbace22d227f6.js"
 );
 
 workbox.clientsClaim();
